88774a4c653e6127cebb9ff20fb57b1e6b6f09b6
margaretlyon21 <156958807+margaretlyon21@users.noreply.github.com>
2024-01-24 15:28:55 -0500

    lab2 functions.cpp

--- lab2 functions.cpp
+++ lab2 functions.cpp
@@ -1,1 +1,74 @@
+#include <iostream>
+using namespace std;
 
+
+class player{
+    private:
+    string Name;
+    int ShotsTaken;
+    int ShotsMade;
+    int PassesAttempted;
+    int PassesMade;
+
+    public:
+
+    // Getters for private variables
+    string getName(){
+        return Name;};
+    int getShotsTaken(){
+        return ShotsTaken;};
+    int getShotsMade(){
+        return ShotsMade;};
+    int getPassesAttempted(){
+        return PassesAttempted;};
+    int getPassesMade(){
+        return PassesMade;};
+    
+    //Setters for private variables 
+    void setShotsTaken(int a){
+        ShotsTaken += a;
+    };
+    void setShotsMade(int a){
+        ShotsMade += a;
+    };
+    void setPassesAttempted(int a){
+        PassesAttempted += 1;
+    };
+    void setPassesMade(int a){
+        PassesMade += 1;
+    }
+
+};
+
+int main(){
+// Asks the user to choose an option, moves to what they chose
+    cout << "Choose an Action:\n 1. Shoot \n 2.Pass \n 3. See Player Stats \n 4. See score";
+    int choice;
+    cin >> choice;
+
+    //shoot
+    if (choice == 1){
+        currentplayer.TakeShot();
+    }
+    //pass
+    else if (choice == 2){
+        currentplayer.PassBall();
+    }
+    //print stats
+    else if (choice == 3){
+        cout << "*****************************************\n";
+        cout << "Player Name: " << currentplayer.getName() << "\n";
+        cout << "Shots Taken: " << currentplayer.getShotsTaken() << "\n";
+        cout << "Shots Made: " << currentplayer.getShotsMade() << "\n";
+        cout << "Passes Attempted: " << currentplayer.getPassesAttempted() << "\n";
+        cout << "Passes Made: " << currentplayer.getPassesMade() << "\n";
+        cout << "******************************************\n";
+    }
+    //print score
+    else if (choice == 4){
+        cout << score1 << "  -  " << score2;
+    }
+
+
+
+}
